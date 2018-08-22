beaaa72f705a77cb96feb64c2f0b5eb26ded44d2
steveTr9 <41507051+steveTr9@users.noreply.github.com>
2018-08-23 00:22:47 +0100

    Add files via upload

--- test_connectz.py
+++ test_connectz.py
@@ -1,40 +1,37 @@
 import connectz
 import unittest
-import numpy as np
 
 
 class TestConnectZ(unittest.TestCase):
 
     def test_s_int(self):
-        self.assertEquals(connectz.s_int(5),5)
-        self.assertEquals(connectz.s_int(''),-1)
-        self.assertEquals(connectz.s_int(0), 0)
+        self.assertEqual(connectz.s_int(5),5)
+        self.assertEqual(connectz.s_int(''),-1)
+        self.assertEqual(connectz.s_int(0), 0)
 
-
-    def test_check_slot(self):
-        self.assertEquals(connectz.check_slot(1, np.array([[0, 0, 0],
-                                                           [1, 0, 0],
-                                                           [1, 2, 0]])),1)
-        self.assertEquals(connectz.check_slot(0, np.array([[0, 0, 0],
-                                                           [1, 0, 0],
-                                                           [1, 2, 0]])), 0)
-        self.assertEquals(connectz.check_slot(2, np.array([[0, 0, 0],
-                                                           [1, 0, 0],
-                                                           [1, 2, 0]])), 2)
+    def test_check_win_swne(self):
+        self.assertEqual(connectz.check_win_swne(7, {0:[1,2,0],
+                                                      1:[2,1,0],
+                                                      2:[1,2,1]}, 1, 1, 1, 3, 3), 1)
 
     def test_check_win(self):
-        self.assertEquals(connectz.check_win(7, np.array([[0, 0, 1],
-                                                          [2, 1, 2],
-                                                          [1, 2, 1]]), 1, 1, 1, 3), 1)
-        self.assertEquals(connectz.check_win(8, np.array([[2, 0, 1],
-                                                          [2, 2, 1],
-                                                          [1, 1, 2]]), 2, 0, 0, 3), 2)
-        self.assertEquals(connectz.check_win(5, np.array([[0, 0, 0],
-                                                          [2, 2, 0],
-                                                          [1, 1, 1]]), 1, 2, 2, 3), 1)
-        self.assertEquals(connectz.check_win(5, np.array([[1, 0, 0],
-                                                          [1, 2, 0],
-                                                          [1, 2, 0]]), 1, 0, 0, 3), 1)
+        self.assertEqual(connectz.check_win(7, {0:[1,2,0],1:[2,1,0],2:[1,2,1]}, 1, 1, 1, 3, 3), 1)
+        self.assertEqual(connectz.check_win(8, {0:[1,2,2],
+                                                 1:[1,2,0],
+                                                 2:[2,1,1]}, 2, 0, 2, 3, 3), 2)
+        self.assertEqual(connectz.check_win(5, {0:[1,2,0],
+                                                 1:[1,2,0],
+                                                 2:[1,0,0]}, 1, 2, 0, 3, 3), 1)
+        self.assertEqual(connectz.check_win(5, {0:[1,1,1],
+                                                 1:[2,2,0],
+                                                 2:[0,0,0]}, 1, 0, 2, 3, 3), 1)
+        self.assertEqual(connectz.check_win(18, {0:[1,2,0,0,0,0],
+                                                  1:[1,2,1,0,0,0],
+                                                  2:[2,1,2,0,0,0],
+                                                  3:[1,2,2,2,1,0],
+                                                  4:[1,1,1,2,2,0],
+                                                  5:[0,0,0,0,0,0],
+                                                  6:[0,0,0,0,0,0]},2,4,4,4,7),2)
 
 
 if __name__ == '__main__':
